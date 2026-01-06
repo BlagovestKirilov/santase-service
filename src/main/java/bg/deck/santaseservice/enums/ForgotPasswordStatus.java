80fc4c0317ad6f4c493579048af514f44784019b
Blagovest Kirilov <blagovestkirilov1@gmail.com>
2026-01-06 20:51:18 +0200

    Added forgot password for users that has confirmed email.

--- src/main/java/bg/deck/santaseservice/enums/ForgotPasswordStatus.java
+++ src/main/java/bg/deck/santaseservice/enums/ForgotPasswordStatus.java
@@ -1,5 +1,5 @@
 package bg.deck.santaseservice.enums;
 
 public enum ForgotPasswordStatus {
-    PENDING, SUCCESS
+    PENDING, EXPIRED, SUCCESS
 }
--- src/main/java/bg/deck/santaseservice/repository/ForgotPasswordRepository.java
+++ src/main/java/bg/deck/santaseservice/repository/ForgotPasswordRepository.java
@@ -1,11 +1,16 @@
 package bg.deck.santaseservice.repository;
 
+import bg.deck.santaseservice.enums.ForgotPasswordStatus;
 import bg.deck.santaseservice.model.ForgotPassword;
+import bg.deck.santaseservice.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface ForgotPasswordRepository extends JpaRepository<ForgotPassword, UUID> {
-    Optional<ForgotPassword> findByForgotPasswordToken(String token);
+    Optional<ForgotPassword> findByForgotPasswordTokenAndStatus(String token, ForgotPasswordStatus status);
+
+    List<ForgotPassword> findAllByUserAndStatus(User user, ForgotPasswordStatus status);
 }
